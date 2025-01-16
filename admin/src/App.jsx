59a88ec094aa61b5c57b50959fb8c9d8e221adbd
ChamodyaIshani99 <138350748+ChamodyaIshani99@users.noreply.github.com>
2025-01-16 16:46:28 +0530

    create Admin panel using react

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
 import { Routes,Route } from 'react-router-dom'
-import Add from './Pages/Add/Add'
+import  Add from './Pages/Add/Add'
 import Order from './Pages/Orders/Order'
 import List from './Pages/List/List'
+import { ToastContainer} from 'react-toastify';
 
 const App = () => {
+
+const url="http://localhost:4000"
+
   return (
     <div>
+      <ToastContainer/>
       <Navbar/>
       <hr />
       <div className="app-content">
         <Sidebar/>
         <Routes>
-          <Route path="/add" element={<Add/>}/>
-          <Route path="/list" element={<List/>}/>
-          <Route path="/orders" element={<Order/>}/>
+          <Route path="/add" element={<Add url={url}/>}/>
+          <Route path="/list" element={<List url={url}/>}/>
+          <Route path="/orders" element={<Order url={url}/>}/>
         </Routes>
       </div>
     </div>
